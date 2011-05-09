cc2282783d9693374e4cd7453fdad87cf35b47e4
Matt Wilson <mhw@hypomodern.com>
2011-05-09 17:09:02 -0400

    Specifying a new version of HTTParty so that we can disable rails-style handling of multiple-values-for-one-key uri strings. Now allowing the 'options' parameter to #get to be a query string. Updated README.md. My text editor hates extraneous whitespace.

--- lib/brightcove-api.rb
+++ lib/brightcove-api.rb
@@ -6,74 +6,82 @@ require 'orderedhash'
 module Brightcove
   class API
     include HTTParty
-    
+    disable_rails_query_string_format
+
     VERSION = '1.0.7'.freeze
-    
+
     DEFAULT_HEADERS = {
       'User-Agent' => "brightcove-api gem #{VERSION}"
     }
-    
+
     headers(DEFAULT_HEADERS)
-        
+
     READ_API_URL = 'http://api.brightcove.com/services/library'
     WRITE_API_URL = 'http://api.brightcove.com/services/post'
-    
+
     attr_accessor :read_api_url
     attr_accessor :write_api_url
     attr_accessor :token
-        
+
     # Brightcove returns text/html as the Content-Type for a response even though the response is JSON.
     # So, let's just parse the response as JSON
     format(:json)
-    
-    # Initialize with your API token  
+
+    # Initialize with your API token
     def initialize(token, read_api_url = READ_API_URL, write_api_url = WRITE_API_URL)
       @token = token
       @read_api_url = read_api_url
       @write_api_url = write_api_url
     end
-    
+
     def debug(location = $stderr)
       self.class.debug_output(location)
     end
-        
+
     def set_http_headers(http_headers = {})
       http_headers.merge!(DEFAULT_HEADERS)
       headers(http_headers)
     end
-    
+
     def set_timeout(timeout)
       default_timeout(timeout)
     end
-    
-    # Call Brightcove using a particular API method, api_method. The options hash is where you can add any parameters appropriate for the API call.
-    def get(api_method, options = {})
-      options.merge!({:command => api_method})
-      options.merge!({:token => @token})
-      query = {}
-      query.merge!({:query => options})
 
-      self.class.get(@read_api_url, query)
+    def build_query_from_options(api_method, options = {})
+      # normalize options to a hash
+      unless options.respond_to?(:merge!)
+        options = CGI.parse(options)
+      end
+      options.merge!({:command => api_method, :token => @token})
+      { :query => options }
     end
-    
+
+    # Call Brightcove using a particular API method, api_method.
+    # The options parameter can be either a query string or a hash. In either case, it is where
+    # you can add any parameters appropriate for the API call. If a query string, it will be
+    # normalized to a hash via CGI.parse.
+    def get(api_method, options = {})
+      self.class.get(@read_api_url, build_query_from_options(api_method, options))
+    end
+
     # Post to Brightcove using a particular API method, api_method. The parameters hash is where you add all the required parameters appropriate for the API call.
-    def post(api_method, parameters = {}) 
+    def post(api_method, parameters = {})
       parameters.merge!({"token" => @token})
-      
+
       body = {}
-      body.merge!({:method => api_method})          
+      body.merge!({:method => api_method})
       body.merge!({:params => parameters})
-            
+
       self.class.post(@write_api_url, {:body => {:json => JSON.generate(body)}})
     end
-    
+
     def post_file(api_method, file, parameters = {})
       parameters.merge!({"token" => @token})
 
       body = {}
-      body.merge!({:method => api_method})          
+      body.merge!({:method => api_method})
       body.merge!({:params => parameters})
-      
+
       # Brightcove requires that the JSON-RPC call absolutely
       # be the first part of a multi-part POST like create_video.
       if RUBY_VERSION >= '1.9'
@@ -81,12 +89,12 @@ module Brightcove
       else
         payload = OrderedHash.new
       end
-      
+
       payload[:json] = body.to_json
       payload[:file] = File.new(file, 'rb')
-          
+
       response = RestClient.post(@write_api_url, payload, :content_type => :json, :accept => :json, :multipart => true)
-      
+
       JSON.parse(response)
     end
   end
